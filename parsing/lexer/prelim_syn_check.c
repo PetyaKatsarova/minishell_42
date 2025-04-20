f3799e6e5d7ea2b6557da91667b146da95c604a4
jstuhrin <codam.2y78l@simplelogin.com>
2025-04-20 10:15:51 +0200

    improved prelim_syn_check: 1. fixed bug caused by empty input and only-whitespace input 2. cleaned up code and made norminette conform

--- parsing/lexer/prelim_syn_check.c
+++ parsing/lexer/prelim_syn_check.c
@@ -1,43 +1,52 @@
 
 #include "../../includes/parsing.h"
 
-// static int	check_pipes(char **input)
-// {
-// 	bool	pipe_flag;
-
-// 	pipe_flag = true;
-// 	while (**input && **input != '\'' && **input != '\"')
-// 	{
-// 		if (**input == '|')
-// 		{
-// 			if (pipe_flag == true)
-// 			{
-// 				return (-2);
-// 			}
-// 			else
-// 			{
-// 				pipe_flag = true;
-// 			}
-// 		}
-// 		else if (is_whitespace(**input) == false)
-// 		{
-// 			pipe_flag = false;
-// 		}
-// 		(*input)++;
-// 	}
-// 	if (pipe_flag == true)
-// 	{
-// 		return (-2);
-// 	}
-// 	return (0);
-// }
-
 /*
 ** this function uses the logic of a state machine. the machine changes its states
 ** based on its current state and the character it encounters. if it is not in the correct
 ** state when reaching the end of the input string (OUTSIDE), the machine returns -1 to
 ** indicate an error ("open quotes").
 */
+
+static int	check_if_empty(char *input)
+{
+	if (*input == '\0')
+	{
+		return (-1);
+	}
+	return (0);
+}
+
+static int	check_if_only_whitespace(char *input)
+{
+	while (is_whitespace(*input))
+	{
+		input++;
+	}
+	if (*input == '\0')
+	{
+		return (-2);
+	}
+	return (0);
+}
+
+static int check_outside(e_state *state, bool *pipe_flag, char input)
+{
+	if (input == '|')
+	{
+		if (*pipe_flag == true)
+			return (-3);
+		else
+			*pipe_flag = true;
+	}
+	else if (is_whitespace(input) == false)
+		*pipe_flag = false;
+	if (input == '\'')
+		*state = INSIDE_SINGLES;
+	else if (input == '\"')
+		*state = INSIDE_DOUBLES;
+	return (0);
+}
 
 static int	scan_input(char *input)
 {
@@ -50,19 +59,8 @@ static int	scan_input(char *input)
 	{
 		if (state == OUTSIDE)
 		{
-			if (*input == '|')
-			{
-				if (pipe_flag == true)
-					return (-2);
-				else
-					pipe_flag = true;
-			}
-			else if (is_whitespace(*input) == false)
-				pipe_flag = false;
-			if (*input == '\'')
-				state = INSIDE_SINGLES;
-			else if (*input == '\"')
-				state = INSIDE_DOUBLES;
+			if (check_outside(&state, &pipe_flag, *input) < 0)
+				return (-3);
 		}
 		else if (state == INSIDE_SINGLES && *input == '\'')
 			state = OUTSIDE;
@@ -71,9 +69,9 @@ static int	scan_input(char *input)
 		input++;
 	}
 	if (pipe_flag == true)
-		return (-2);
+		return (-3);
 	if (state != OUTSIDE)
-		return (-1);
+		return (-4);
 	return (0);
 }
 
@@ -81,16 +79,20 @@ int	prelim_syn_check(char *input)
 {
 	int	res;
 
-	res = scan_input(input);
-	if (res == -1)
-	{
-		write(1, "syntax error: open quotes\n", 26);
+	if (check_if_empty(input) < 0)
 		return (-1);
-	}
-	if (res == -2)
+	if (check_if_only_whitespace(input) < 0)
+		return (-2);
+	res = scan_input(input);
+	if (res == -3)
 	{
 		write(1, "syntax error: misplaced '|'\n", 28);
-		return (-2);
+		return (-3);
+	}
+	if (res == -4)
+	{
+		write(1, "syntax error: open quotes\n", 26);
+		return (-4);
 	}
 	return (0);
 }
