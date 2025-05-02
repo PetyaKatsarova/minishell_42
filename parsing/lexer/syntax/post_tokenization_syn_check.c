aa040c926e5647b70d078ea40298dd52f0d15a27
jstuhrin <codam.2y78l@simplelogin.com>
2025-05-02 16:41:26 +0200

    refactored post_tokenization_syn_check for norminette

--- parsing/lexer/syntax/post_tokenization_syn_check.c
+++ parsing/lexer/syntax/post_tokenization_syn_check.c
@@ -13,6 +13,27 @@
 #include "../../../includes/parsing.h"
 #include "../../../includes/minishell.h"
 
+static int	error_check(t_token *current, t_env_list *env_list, bool *redir_flag)
+{
+	if (is_redir(current->token_type))
+	{
+		env_list->last_exit_status = 2;
+		write(2, "syntax error: unexpected redir\n", 32);
+		return (2);
+	}
+	else if (current->token_type == PIPE)
+	{
+		env_list->last_exit_status = 2;
+		write(2, "syntax error: unexpected '|'\n", 30);
+		return (2);
+	}
+	else
+	{
+		*redir_flag = false;
+	}
+	return (0);
+}
+
 int	post_tokenization_syn_check(t_token *current, t_env_list *env_list)
 {
 	bool	redir_flag;
@@ -22,21 +43,9 @@ int	post_tokenization_syn_check(t_token *current, t_env_list *env_list)
 	{
 		if (redir_flag == true)
 		{
-			if (is_redir(current->token_type))
+			if (error_check(current, env_list, &redir_flag) == 2)
 			{
-				env_list->last_exit_status = 2;
-				write(2, "syntax error: unexpected redir\n", 32);
 				return (2);
-			}
-			else if (current->token_type == PIPE)
-			{
-				env_list->last_exit_status = 2;
-				write(2, "syntax error: unexpected '|'\n", 30);
-				return (2);
-			}
-			else
-			{
-				redir_flag = false;
 			}
 		}
 		if (is_redir(current->token_type) == true)
