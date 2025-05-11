eed841f530535907421cf7df9a5e17baa68b919a
jstuhrin <codam.2y78l@simplelogin.com>
2025-05-11 11:10:08 +0200

    cleanup in signals/ and Makefile

--- signals/signals_heredoc.c
+++ signals/signals_heredoc.c
@@ -21,16 +21,16 @@ int	setup_sigint_heredoc(void)
 	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
 	sigaddset(&sa.sa_mask, SIGINT);
-	sigaddset(&sa.sa_mask, SIGQUIT);
+	//sigaddset(&sa.sa_mask, SIGQUIT);
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 	{
 		perror("sigaction for SIGINT");
 		return(-1);
 	}
-	if (sigaction(SIGQUIT, &sa, NULL) == -1)
-	{
-		perror("sigaction for SIGQUIT");
-		return (-1);
-	}
+	// if (sigaction(SIGQUIT, &sa, NULL) == -1)
+	// {
+	// 	perror("sigaction for SIGQUIT");
+	// 	return (-1);
+	// }
 	return (0);
 }
