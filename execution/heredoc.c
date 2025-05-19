11f743f82cc0f78a1a8ebf3c197306eb9cfd61e4
jstuhrin <codam.2y78l@simplelogin.com>
2025-05-19 07:53:22 +0200

    fixed leak in heredoc.c

--- execution/heredoc.c
+++ execution/heredoc.c
@@ -20,17 +20,17 @@ int	apply_heredoc(t_node *redir, int i)
 	name = ft_itoa(i);
 	fd = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
-		return (perror_and_return(name));
+		return (free(name), perror_and_return("apply_heredoc"));
 	write(fd, redir->heredoc_str, ft_strlen(redir->heredoc_str));
 	close(fd);
 	fd = open(name, O_RDONLY);
 	if (fd < 0)
-		return (perror_and_return(name));
+		return (free(name), perror_and_return("apply_heredoc"));
 	unlink(name);
 	if (dup2(fd, STDIN_FILENO) == -1)
 	{
 		close(fd);
-		return (EXIT_FAILURE);
+		return (free(name), perror_and_return("apply_heredoc"));
 	}
 	close(fd);
 	free(name);
